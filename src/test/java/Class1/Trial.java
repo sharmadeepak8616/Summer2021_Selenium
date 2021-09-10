d83be877664e2351d726f23a82cd5851c12f4978
Deepak Sharma <sharma.deepak8616@gmail.com>
2021-09-09 21:36:44 -0400

    created trial file

---
+++ src/test/java/Class1/Trial.java
@@ -0,0 +1,21 @@
+package Class1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Trial {
+    public static void main(String[] args) {
+
+        // path of the driver (chrome)
+        System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver");      // Mac
+
+        // System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");   // Windows
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.facebook.com/");
+
+
+
+    }
+}
--- src/test/java/RandomClass.java
+++ src/test/java/RandomClass.java
@@ -1,8 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class RandomClass {
     public static void main(String[] args) {
 
 
         System.out.println("Hello World!");
+
+        List<Integer> myList = new ArrayList<Integer>();
+
+
 
     }
 }
